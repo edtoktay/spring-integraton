c59f83ac16fb363264519436c5fa9ec2fe195dc7
E. Deniz TOKTAY <edeniztoktay@gmail.com>
2017-06-02 14:38:34 +0300

    Update ProcessFile.java

--- src/main/java/org/edtoktay/spring_integration/file_ops/ProcessFile.java
+++ src/main/java/org/edtoktay/spring_integration/file_ops/ProcessFile.java
@@ -13,10 +13,6 @@ import org.springframework.integration.file.FileHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonToken;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 /**
  * @author deniz.toktay
  *
@@ -28,24 +24,11 @@ public class ProcessFile {
 	FileProperties fileProperties;
 	@Autowired EmailSender emailSender;
 
-	public void isValidJSON(String json) throws Exception {
-		JsonParser jsonParser = new ObjectMapper().getFactory().createParser(json);
-		while (jsonParser.nextToken() != JsonToken.END_OBJECT) {
-			System.out.println(jsonParser.nextToken());
-		}
-	}
-
 	public void process(Message<String> message) throws Exception {
 		File file = message.getHeaders().get(FileHeaders.ORIGINAL_FILE, File.class);
 		String fileName = (String) message.getHeaders().get("file_name");
 		String content = message.getPayload();
 		System.out.println(String.format("%s receive. Content: %s", fileName, content));
-		try {
-			isValidJSON(content);
-			file.renameTo(new File(fileProperties.getSuccessPath(), file.getName()));
-		} catch (Exception e) {
-			file.renameTo(new File(fileProperties.getFailPath(), file.getName()));
-		}
 		emailSender.sendEmail();
 	}
 }
